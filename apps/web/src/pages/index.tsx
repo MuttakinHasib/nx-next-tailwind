bb5c88013ab4373def34b2f93eee3a44ae1fb117
Hasib Molla <hasibmolla28@gmail.com>
2022-07-28 16:57:00 +0600

    feat: :sparkles: Add next.js & setup tailwind css\

---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container py-15">
+      <h1 className="text-xl text-green-500">Hello Tanimur</h1>
+    </div>
+  );
+};
+
+export default Home;
